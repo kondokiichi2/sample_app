576f12062760396f9d4435b7149d341df628f423
kondokiichi2 <kiichikiichi0708@gmail.com>
2026-09-08 17:36:49 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,6 +26,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_select "title", "Example User | Ruby on Rails Tutorial Sample App"
+    assert is_logged_in?
     assert_not flash.empty?
   end
 end
