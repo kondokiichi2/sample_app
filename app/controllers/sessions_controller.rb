aec7ce66d351cc899780482cb30fa3e865e6b898
kondokiichi2 <kiichikiichi0708@gmail.com>
2026-09-12 18:44:38 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])  
-      reset_session      # ログインの直前に必ずこれを書くこと
-      log_in user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      reset_session
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user  
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
@@ -16,7 +18,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
+
 end
