e91d39dd50e9232930a98f849ae4e34bf5013591
kondokiichi2 <kiichikiichi0708@gmail.com>
2026-09-14 04:16:25 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_response :success
@@ -11,9 +15,27 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+
+    # 未ログイン時: ログインリンクは表示され、ログイン専用リンクは表示されない
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", users_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+    assert_select "a[href=?]", logout_path,     count: 0
+
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    log_in_as(@user)
+    get root_path
+
+    # ログイン時: ログインリンクは表示されず、ログイン専用リンクが表示される
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -98,3 +98,25 @@ class RememberingTest < UsersLogin
     assert cookies[:remember_token].blank?
   end
 end
+
+class FriendlyForwardingTest < UsersLogin
+
+  test "forwarding url is used only on the first login after the request" do
+    get edit_user_path(@user)
+    assert_redirected_to login_url
+    assert_equal edit_user_url(@user), session[:forwarding_url]
+
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    follow_redirect!
+    assert_response :success
+
+    # ログイン後はforwarding_urlがリセットされているはず
+    assert_nil session[:forwarding_url]
+
+    delete logout_path
+    log_in_as(@user)
+    # 転送先が無いので、デフォルトのプロフィール画面にリダイレクトされる
+    assert_redirected_to @user
+  end
+end
